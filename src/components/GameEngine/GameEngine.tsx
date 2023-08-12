6b6baa28d515f6a88d2f285dca057080d43eaeec
kosei <abdelhamid.boudjit@gmail.com>
2023-08-12 23:29:15 +0100

    Update GameEngine.tsx

--- src/components/GameEngine/GameEngine.tsx
+++ src/components/GameEngine/GameEngine.tsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import Letter from "../Letter/Letter";
 import { ArrayUtils } from "../../shared";
 
@@ -14,19 +14,35 @@ function GameEngine({
   const [formedWord, setFormedWord] = useState("");
   const [row, setRow] = useState(0);
   const [col, setColumn] = useState(0);
-  const columns = ArrayUtils.createArray(word.length);
-  const rows = ArrayUtils.createArray(7 - difficulty * 2);
-  const currentPositon = [row, col];
+  const [currentPosition, setCurrentPosition] = useState<[number, number]>([
+    row,
+    col,
+  ]);
+  const colLength = ArrayUtils.createArray(word.length);
+  const rowLength = ArrayUtils.createArray(7 - difficulty * 2);
+  const handleLetterChange = (l: string) => {
+    setFormedWord(formedWord + l);
+    if (row === rowLength.length - 1) {
+      setRow(0);
+      setColumn((p) => p + 1);
+    } else setRow((p) => p + 1);
+  };
+
+  useEffect(() => {
+    setCurrentPosition([row, col]);
+  }, [col, row]);
   return (
     <div className="flex flex-col items-center justify-center gap-3">
-      {rows.map((_, i) => {
+      {rowLength.map((_, i) => {
         return (
           <div className="flex items-center justify-center gap-3">
-            {columns.map((_, j) => {
+            {colLength.map((_, j) => {
               const letterPosition = [i, j];
-              return (
-                <Letter focus onChange={(l) => setFormedWord(formedWord + l)} />
+              const current = ArrayUtils.compare(
+                currentPosition,
+                letterPosition
               );
+              return <Letter focus={current} onChange={handleLetterChange} />;
             })}
           </div>
         );

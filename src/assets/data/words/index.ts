a55da3f9d106b6149d560a69770bd93093748726
kosei <abdelhamid.boudjit@gmail.com>
2023-08-12 22:03:00 +0100

    Update index.ts

--- src/assets/data/words/index.ts
+++ src/assets/data/words/index.ts
@@ -1,14 +1,7 @@
-import { three as t } from "./three";
-import { four as fo } from "./four";
-import { five as fi } from "./five";
-import { six as si } from "./six";
-import { seven as se } from "./seven";
-import { eight as e } from "./eight";
-import { StringUtils } from "../../../shared";
-
-export const three = StringUtils.splitChunks(t, 3);
-export const four = StringUtils.splitChunks(fo, 4);
-export const five = StringUtils.splitChunks(fi, 5);
-export const six = StringUtils.splitChunks(si, 6);
-export const seven = StringUtils.splitChunks(se, 7);
-export const eight = StringUtils.splitChunks(e, 8);
+import { three } from "./three";
+import { four } from "./four";
+import { five } from "./five";
+import { six } from "./six";
+import { seven } from "./seven";
+import { eight } from "./eight";
+export const wordsArr = [three, four, five, six, seven, eight];

aa6076a5ebe75499ff942ae145da83d04fc1ffed
kosei <abdelhamid.boudjit@gmail.com>
2023-08-12 15:32:29 +0100

    Update Nav.tsx

--- src/components/Nav/Nav.tsx
+++ src/components/Nav/Nav.tsx
@@ -1,10 +1,26 @@
+import { IconBrandGithub, IconInfoSmall } from "@tabler/icons-react";
+import IconButton from "../../ui/buttons/IconButton";
 import Logo from "../Logo/Logo";
+import DarkModeButton from "../DarkMode/DarkMode";
+import Container from "../../ui/layout/Container";
 
 function Nav() {
   return (
-    <nav>
-      <Logo height={80} />
-      <div></div>
+    <nav className="w-screen">
+      <Container className=" flex items-center justify-between">
+        <Logo height={80} />
+        <div>
+          <IconButton title="how to play?">
+            <IconInfoSmall />
+          </IconButton>
+          <IconButton title="Github repo">
+            <IconBrandGithub></IconBrandGithub>
+          </IconButton>
+          <IconButton title="how to play?">
+            <DarkModeButton />
+          </IconButton>
+        </div>
+      </Container>
     </nav>
   );
 }

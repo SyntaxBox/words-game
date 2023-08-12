d0d537d5ac4485abb53c278d5043f4ca29bace3f
kosei <abdelhamid.boudjit@gmail.com>
2023-08-12 14:16:01 +0100

    Update index.ts

--- src/assets/data/words/index.ts
+++ src/assets/data/words/index.ts
@@ -4,3 +4,11 @@ import { five as fi } from "./five";
 import { six as si } from "./six";
 import { seven as se } from "./seven";
 import { eight as e } from "./eight";
+import { StringUtils } from "../../../shared";
+
+export const three = StringUtils.splitChunks(t, 3);
+export const four = StringUtils.splitChunks(fo, 4);
+export const five = StringUtils.splitChunks(fi, 5);
+export const six = StringUtils.splitChunks(si, 6);
+export const seven = StringUtils.splitChunks(se, 7);
+export const eight = StringUtils.splitChunks(e, 8);

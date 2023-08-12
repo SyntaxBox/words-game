dde2daff311199fde3f9aca9004b9d1fddd6d4b7
kosei <abdelhamid.boudjit@gmail.com>
2023-08-12 22:03:09 +0100

    Update Logo.tsx

--- src/components/Logo/Logo.tsx
+++ src/components/Logo/Logo.tsx
@@ -1,13 +1,15 @@
+import { Link } from "react-router-dom";
+
 function Logo({ height }: { height: number }) {
   const style = {
     height: `${height}px`,
     width: `${height}px`,
   };
   return (
-    <div className="flex items-center justify-between">
+    <Link to="/" className="flex items-center justify-between">
       <img style={style} src="./logo.svg" alt="logo image" />
       <p className="text-3xl font-bold text-pink-500">ords</p>
-    </div>
+    </Link>
   );
 }
 

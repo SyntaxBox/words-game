788e9474046f2548aa26680b7a6b50111d34fb19
kosei <abdelhamid.boudjit@gmail.com>
2023-08-12 23:17:10 +0100

    Update GameEngine.tsx

--- src/components/GameEngine/GameEngine.tsx
+++ src/components/GameEngine/GameEngine.tsx
@@ -1,5 +1,6 @@
 import { useState } from "react";
 import Letter from "../Letter/Letter";
+import { ArrayUtils } from "../../shared";
 
 function GameEngine({
   difficulty,
@@ -11,11 +12,25 @@ function GameEngine({
   word: string;
 }) {
   const [formedWord, setFormedWord] = useState("");
-  const columns = word.length;
-  const rows = 7 - difficulty * 2;
+  const [row, setRow] = useState(0);
+  const [col, setColumn] = useState(0);
+  const columns = ArrayUtils.createArray(word.length);
+  const rows = ArrayUtils.createArray(7 - difficulty * 2);
+  const currentPositon = [row, col];
   return (
-    <div>
-      <Letter Value={(l) => setFormedWord(formedWord + l)} />
+    <div className="flex flex-col items-center justify-center gap-3">
+      {rows.map((_, i) => {
+        return (
+          <div className="flex items-center justify-center gap-3">
+            {columns.map((_, j) => {
+              const letterPosition = [i, j];
+              return (
+                <Letter focus onChange={(l) => setFormedWord(formedWord + l)} />
+              );
+            })}
+          </div>
+        );
+      })}
     </div>
   );
 }

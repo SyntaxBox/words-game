1ac00adf5ba7ef82fcf950dcecad5680e27d7c48
kosei <abdelhamid.boudjit@gmail.com>
2023-08-12 23:23:52 +0100

    add two arrays comparison

--- src/shared/utils/ArrayUtils.ts
+++ src/shared/utils/ArrayUtils.ts
@@ -2,4 +2,7 @@ export class ArrayUtils {
   public static createArray(number: number) {
     return Array.from({ length: number }, (_, index) => index);
   }
+  public static compare(array1: Array<unknown>, array2: Array<unknown>) {
+    return JSON.stringify(array1) === JSON.stringify(array2);
+  }
 }

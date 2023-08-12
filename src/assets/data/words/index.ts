86fe27678775436fb393c94d82bcd69ab77d6f87
kosei <abdelhamid.boudjit@gmail.com>
2023-08-12 14:12:58 +0100

    replacement of json files

---
+++ src/assets/data/words/index.ts
@@ -0,0 +1,6 @@
+import { three as t } from "./three";
+import { four as fo } from "./four";
+import { five as fi } from "./five";
+import { six as si } from "./six";
+import { seven as se } from "./seven";
+import { eight as e } from "./eight";

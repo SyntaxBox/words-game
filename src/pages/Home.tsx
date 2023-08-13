cdebe44ce81c7f9c94307160db4d15ece644b6c4
kosei <abdelhamid.boudjit@gmail.com>
2023-08-13 13:55:54 +0100

    add word length selection

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,28 +1,34 @@
 import { useState } from "react";
-import Tabs from "../components/Tabs/Tabs";
-import Container from "../ui/layout/Container";
-import H1 from "../ui/typography/H1";
-import P from "../ui/typography/P";
-import TextButton from "../ui/buttons/textButton";
-import A from "../ui/typography/A";
 import { Link } from "react-router-dom";
+import Tabs from "../components/Tabs/Tabs";
+import { H1, Container, A, TextButton } from "../ui";
+import Range from "../components/Range/Range";
 
 function Home() {
-  const [currentTab, setCurrentTab] = useState<0 | 1 | 2>(0);
+  const [difficulty, setDifficulty] = useState<number>(0);
+  const [wordLength, setWordLength] = useState<number>(3);
   return (
-    <Container className="flex flex-col items-center justify-between gap-12 mt-16">
+    <Container className="flex flex-col items-center justify-between gap-12 mt-10">
       <H1>
         Welcome To <span className="text-pink-500">Words</span> Game
       </H1>
-      <div className="flex flex-col gap-3">
-        <P>Select Difficulty</P>
+      <div className="flex flex-col items-start justify-center gap-6">
         <Tabs
           tabs={["Easy", "Medium", "Hard"]}
-          currentTab={currentTab}
-          setCurrentTab={setCurrentTab}
+          currentTab={difficulty}
+          text="Select Difficulty:"
+          setCurrentTab={setDifficulty}
+        />
+        <Range
+          min={0}
+          max={5}
+          value={wordLength}
+          correctionFactor={3}
+          setValue={setWordLength}
+          text="Select Word Length:"
         />
       </div>
-      <Link to="/game" state={{ difficulty: currentTab }}>
+      <Link to="/game" state={{ difficulty, wordLength }}>
         <TextButton>Start!</TextButton>
       </Link>
       <A>Don't know how to play ?</A>

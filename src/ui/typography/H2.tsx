aa1aca38f7bbe285980ee3763a6fc33dd6b1cb7f
kosei <abdelhamid.boudjit@gmail.com>
2023-08-13 13:53:00 +0100

    add export

--- src/ui/typography/H2.tsx
+++ src/ui/typography/H2.tsx
@@ -1,4 +1,4 @@
-function H2({ children }: { children: React.ReactNode }) {
+export function H2({ children }: { children: React.ReactNode }) {
   return (
     <h2 className="text-3xl md:text-4xl font-bold text-center dark:text-white">
       {children}

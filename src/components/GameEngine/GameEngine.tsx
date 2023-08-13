5fd02610b43962d3f5523188876b6a74b157f915
kosei <abdelhamid.boudjit@gmail.com>
2023-08-13 18:25:42 +0100

    Update GameEngine.tsx

--- src/components/GameEngine/GameEngine.tsx
+++ src/components/GameEngine/GameEngine.tsx
@@ -1,6 +1,6 @@
-import { useEffect, useState } from "react";
-import Letter from "../Letter/Letter";
+import { useState } from "react";
 import { ArrayUtils } from "../../shared";
+import Word from "../Word/Word";
 
 function GameEngine({
   difficulty,
@@ -11,40 +11,22 @@ function GameEngine({
   setScore: (score: number) => unknown;
   word: string;
 }) {
-  const [formedWord, setFormedWord] = useState("");
-  const [row, setRow] = useState(0);
-  const [col, setColumn] = useState(0);
-  const [currentPosition, setCurrentPosition] = useState<[number, number]>([
-    row,
-    col,
-  ]);
-  const colLength = ArrayUtils.createArray(word.length);
   const rowLength = ArrayUtils.createArray(7 - difficulty * 2);
-  const handleLetterChange = (l: string) => {
-    setFormedWord(formedWord + l);
-    if (row === rowLength.length - 1) {
-      setRow(0);
-      setColumn((p) => p + 1);
-    } else setRow((p) => p + 1);
+  const [currentRow, setCurrentRow] = useState(0);
+  const handleChange = (res: boolean) => {
+    if (res) "console.log(res);";
+    else setCurrentRow((p) => p + 1);
   };
-
-  useEffect(() => {
-    setCurrentPosition([row, col]);
-  }, [col, row]);
   return (
     <div className="flex flex-col items-center justify-center gap-3">
       {rowLength.map((_, i) => {
         return (
-          <div className="flex items-center justify-center gap-3">
-            {colLength.map((_, j) => {
-              const letterPosition = [i, j];
-              const current = ArrayUtils.compare(
-                currentPosition,
-                letterPosition
-              );
-              return <Letter focus={current} onChange={handleLetterChange} />;
-            })}
-          </div>
+          <Word
+            onChange={handleChange}
+            focus={currentRow === i}
+            word={word}
+            key={i}
+          />
         );
       })}
     </div>

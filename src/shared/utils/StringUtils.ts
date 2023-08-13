a18cdf041cc173b4d5d0b3560fd6aa3a6221e75e
kosei <abdelhamid.boudjit@gmail.com>
2023-08-13 18:26:24 +0100

    Update StringUtils.ts

--- src/shared/utils/StringUtils.ts
+++ src/shared/utils/StringUtils.ts
@@ -6,4 +6,37 @@ export class StringUtils {
     }
     return result;
   }
+
+  public static isAlphabetLetter(input: string) {
+    // Convert the input to uppercase to handle both upper and lower case letters
+    const uppercaseInput = input.toUpperCase();
+
+    // Check if the input is exactly one character long and it's a letter
+    return uppercaseInput.length === 1 && /[A-Z]/.test(uppercaseInput);
+  }
+  public static compareStrings(str1: string, str2: string) {
+    if (str1.length !== str2.length) {
+      throw new Error("Input strings must have the same length");
+    }
+
+    const result: {
+      correct: boolean;
+      letters: {
+        letter: string;
+        isEqual: boolean;
+      }[];
+    } = {
+      correct: str1 === str2,
+      letters: [],
+    };
+    if (result.correct) return result;
+    for (let i = 0; i < str1.length; i++) {
+      result.letters.push({
+        letter: str2[i],
+        isEqual: str1[i] === str2[i],
+      });
+    }
+
+    return result;
+  }
 }

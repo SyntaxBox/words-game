dda02f4e1f7d68a4eff0084224ae581c1cce94ca
kosei <abdelhamid.boudjit@gmail.com>
2023-08-13 18:26:02 +0100

    Update Game.tsx

--- src/pages/Game.tsx
+++ src/pages/Game.tsx
@@ -3,12 +3,15 @@ import { Container } from "../ui";
 import { useEffect, useState } from "react";
 import GameEngine from "../components/GameEngine/GameEngine";
 import Score from "../components/Score/Score";
+import { ArrayUtils, StringUtils } from "../shared";
+import { wordsArr } from "../assets";
 
 function Game() {
   const location = useLocation();
   const navigate = useNavigate();
 
   const [score, setScore] = useState(0);
+  const [word, setWord] = useState("");
 
   const difficulty = location.state?.difficulty;
   const wordLength = location.state?.wordLength;
@@ -18,13 +21,18 @@ function Game() {
     if (!Number.isInteger(wordLength)) navigate("/");
   }, []);
 
+  const words = StringUtils.splitChunks(wordsArr[wordLength], wordLength + 3);
+  useEffect(() => {
+    setWord(ArrayUtils.randomSelect(words));
+  }, []);
+
   return (
     <Container className="flex gap-4">
       <div className="w-fit">
         <Score score={score} />
       </div>
       <div className="flex items-center justify-center">
-        <GameEngine difficulty={difficulty} setScore={setScore} word="hello" />
+        <GameEngine difficulty={difficulty} setScore={setScore} word={word} />
       </div>
     </Container>
   );

522cb1c5214c0709fdffea400b5968f8f7e32009
kosei <abdelhamid.boudjit@gmail.com>
2023-08-13 13:56:14 +0100

    add word selection state and redirection

--- src/pages/Game.tsx
+++ src/pages/Game.tsx
@@ -1,13 +1,23 @@
-import { useLocation } from "react-router-dom";
-import Container from "../ui/layout/Container";
-import { useState } from "react";
+import { useLocation, useNavigate } from "react-router-dom";
+import { Container } from "../ui";
+import { useEffect, useState } from "react";
 import GameEngine from "../components/GameEngine/GameEngine";
 import Score from "../components/Score/Score";
 
 function Game() {
   const location = useLocation();
+  const navigate = useNavigate();
+
   const [score, setScore] = useState(0);
-  const difficulty = location.state?.difficulty ?? 1;
+
+  const difficulty = location.state?.difficulty;
+  const wordLength = location.state?.wordLength;
+
+  useEffect(() => {
+    if (!Number.isInteger(difficulty)) navigate("/");
+    if (!Number.isInteger(wordLength)) navigate("/");
+  }, []);
+
   return (
     <Container className="flex gap-4">
       <div className="w-fit">

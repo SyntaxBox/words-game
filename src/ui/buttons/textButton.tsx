9cf003fb271bef435a4c048aa46b85b77f705f0b
kosei <abdelhamid.boudjit@gmail.com>
2023-08-13 13:53:19 +0100

    add export

--- src/ui/buttons/textButton.tsx
+++ src/ui/buttons/textButton.tsx
@@ -2,12 +2,13 @@ import { ReactNode } from "react";
 
 type props = {
   children: ReactNode;
+  className?: string;
 } & JSX.IntrinsicElements["button"];
 
-function IconButton({ children, ...rest }: props) {
+export function TextButton({ children, className, ...rest }: props) {
   return (
     <button
-      className="py-4 px-8 inline-flex justify-center items-center gap-2 rounded-md bg-pink-500 border border-transparent font-semibold text-white text-xl  hover:bg-pink-600 focus:outline-none focus:ring-2 ring-offset-white focus:ring-pink-500 focus:ring-offset-2 transition-all dark:focus:ring-offset-gray-800"
+      className={`py-4 px-8 inline-flex justify-center items-center gap-2 rounded-md bg-pink-500 border border-transparent font-semibold text-white text-xl  hover:bg-pink-600 focus:outline-none focus:ring-2 ring-offset-white focus:ring-pink-500 focus:ring-offset-2 transition-all dark:focus:ring-offset-gray-800 ${className}`}
       {...rest}
     >
       {children}
@@ -15,4 +16,4 @@ function IconButton({ children, ...rest }: props) {
   );
 }
 
-export default IconButton;
+export default TextButton;

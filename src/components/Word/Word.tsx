0a9dd9f673b9ec8672cb68f7dbbbf14a1ce89b79
kosei <abdelhamid.boudjit@gmail.com>
2023-08-15 21:06:12 +0100

    Update Word.tsx

--- src/components/Word/Word.tsx
+++ src/components/Word/Word.tsx
@@ -2,6 +2,8 @@ import { useEffect, useState } from "react";
 import Letter from "../Letter/Letter";
 import { ArrayUtils, StringUtils } from "../../shared";
 import Alert from "../Alert/Alert";
+import ShowResult from "../ShowResult/ShowResult";
+import ConfettiExplosion from "react-confetti-explosion";
 
 function Word({
   word,
@@ -12,12 +14,26 @@ function Word({
   word: string;
   focus?: boolean;
   wordsList: string[];
-  onChange: (res: string) => any;
+  onChange: (res: boolean) => any;
 }) {
-  const columns = ArrayUtils.createArray(word.length);
+  const [columns, setColumns] = useState(ArrayUtils.createArray(word.length));
   const [composedWord, setComposedWord] = useState("");
   const [wrongWord, setWrongWord] = useState(false);
   const [currentCol, setCurrentCol] = useState(0);
+  const [result, setResult] = useState<{
+    correct: boolean;
+    letters: {
+      letter: string;
+      status: "correct" | "unplaced" | "wrong";
+    }[];
+  } | null>(null);
+  useEffect(() => {
+    setColumns(ArrayUtils.createArray(word.length));
+    setComposedWord("");
+    setWrongWord(false);
+    setCurrentCol(0);
+    setResult(null);
+  }, [word]);
   const handleLetterChange = (l: string) => {
     if (wrongWord === false) setWrongWord(false);
     if (l === "delete") {
@@ -44,23 +60,50 @@ function Word({
   const handleCompare = () => {
     const isValidWord = wordsList.includes(composedWord.toLocaleLowerCase());
     if (isValidWord) {
-      const diff = StringUtils.compareStrings(word, composedWord.toLowerCase());
-      if (diff.correct) onChange("");
+      setResult(StringUtils.compareStrings(word, composedWord.toLowerCase()));
     } else setWrongWord(true);
   };
 
+  useEffect(() => {
+    const t = setTimeout(() => {
+      if (result !== null) {
+        onChange(result.correct);
+      }
+    }, word.length * 200 + 500);
+    return () => clearTimeout(t);
+  }, [result]);
+
   return (
-    <div className="flex items-center justify-center gap-3">
-      {columns.map((_, i) => {
-        const current = focus && i === currentCol;
-        return <Letter key={i} focus={current} onChange={handleLetterChange} />;
-      })}
+    <div className="flex w-full items-center px-3 justify-center gap-2 sm:gap-3">
+      {!result ? (
+        <>
+          {columns.map((_, i) => {
+            const current = focus && i === currentCol;
+            return (
+              <Letter key={i} focus={current} onChange={handleLetterChange} />
+            );
+          })}
+        </>
+      ) : (
+        <div className="w-full h-full flex flex-col items-center justify-center">
+          <ShowResult duration={word.length * 200} result={result} />
+          {result?.correct && <ConfettiExplosion />}
+        </div>
+      )}
       {wrongWord && (
         <Alert
-          title="Word is not Valid"
-          description="this word is not valid in our words base. please try typing another word"
-          className="absolute top-0 right-0"
-        />
+          title="Warning: Word is not Valid"
+          className="absolute top-0 w-full sm:top-4 sm:right-6 lg:right-16"
+        >
+          <p>Please try typing another word</p>
+          <p>
+            Try for example:{" "}
+            <strong className="text-lg text-bold">
+              {" "}
+              {ArrayUtils.randomSelect(wordsList).toUpperCase()}
+            </strong>
+          </p>
+        </Alert>
       )}
     </div>
   );

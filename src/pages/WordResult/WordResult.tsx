f30e8e6808b0bbce34b56a49767f772507064d7d
kosei <abdelhamid.boudjit@gmail.com>
2023-08-15 21:04:50 +0100

    delete unwanted file

--- src/pages/WordResult/WordResult.tsx
+++
@@ -1,9 +0,0 @@
-import { StringUtils } from "../../shared";
-
-function WordResult({ correct, typed }: { correct: string; typed: string }) {
-  const res = StringUtils.compareStrings(correct, typed);
-  console.log(res);
-  return <div>WordResult</div>;
-}
-
-export default WordResult;

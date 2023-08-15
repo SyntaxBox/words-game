46a7ffc5093ec7dc6aad45b1da154a0e0ec01173
kosei <abdelhamid.boudjit@gmail.com>
2023-08-15 21:06:06 +0100

    move code to ResultBlock

--- src/components/ShowResult/ShowResult.tsx
+++ src/components/ShowResult/ShowResult.tsx
@@ -1,3 +1,5 @@
+import ResultBlock from "./ResultBlock";
+
 function ShowResult({
   result,
   duration,
@@ -15,21 +17,16 @@ function ShowResult({
   const eachLetterDuration = duration / result.letters.length;
   const delay = eachLetterDuration + 20;
   return (
-    <div>
+    <div className="w-full flex gap-2 sm:gap-3 items-center justify-center max-w-[500px]">
       {letters.map(({ letter, status }, i) => {
         const d = delay * i;
         return (
-          <div
-            className={`py-4 px-8 inline-flex justify-center items-center gap-2 rounded-md border border-transparent  font-semibold text-xl transition-all aspect-square bg-pink-100 dark:hover:bg-[#ec489a68] dark:bg-[#ec489a3c] text-pink-500 dark:text-pink-400   ${
-              status === "correct"
-                ? "bg-pink-500"
-                : status === "unplaced"
-                ? "bg-orange-500"
-                : "bg-red-500"
-            }`}
-          >
-            {letter}
-          </div>
+          <ResultBlock
+            key={i}
+            letter={letter.toUpperCase()}
+            status={correct ? "correctWord" : status}
+            delay={d}
+          />
         );
       })}
     </div>

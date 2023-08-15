9032807e303206ab8242205f030d7b3b87e65c4e
kosei <abdelhamid.boudjit@gmail.com>
2023-08-15 21:06:46 +0100

    add gameOver

--- src/pages/Game.tsx
+++ src/pages/Game.tsx
@@ -5,6 +5,7 @@ import GameEngine from "../components/GameEngine/GameEngine";
 import Score from "../components/Score/Score";
 import { ArrayUtils, StringUtils } from "../shared";
 import { wordsArr } from "../assets";
+import GameOver from "../components/GameOver/GameOver";
 
 function Game() {
   const location = useLocation();
@@ -13,36 +14,37 @@ function Game() {
   const [score, setScore] = useState(0);
   const [word, setWord] = useState("");
   const [words, setWords] = useState<string[]>([]);
+  const [gameOver, setGameOver] = useState("");
 
   const difficulty = location.state?.difficulty;
   const wordLength = location.state?.wordLength;
-
   useEffect(() => {
     if (!Number.isInteger(difficulty)) navigate("/");
     if (!Number.isInteger(wordLength)) navigate("/");
   }, []);
 
   useEffect(() => {
-    if (wordLength)
+    if (Number.isInteger(wordLength))
       setWords(StringUtils.splitChunks(wordsArr[wordLength], wordLength + 3));
   }, [wordLength]);
   useEffect(() => {
     if (words) setWord(ArrayUtils.randomSelect(words));
-  }, [words]);
+  }, [words, score]);
   if (!word) return null;
+
   return (
-    <Container className="flex gap-4 relative">
-      <div className="w-fit">
+    <Container className="flex justify-center mt-24">
+      <div className="flex items-center justify-center flex-col gap-6">
         <Score score={score} />
-      </div>
-      <div className="flex items-center justify-center">
         <GameEngine
           difficulty={difficulty}
           setScore={setScore}
           word={word}
           wordsList={words}
+          onGameOver={(word) => setGameOver(word)}
         />
       </div>
+      {gameOver && <GameOver word={gameOver} score={score} />}
     </Container>
   );
 }

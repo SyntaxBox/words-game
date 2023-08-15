f471cd4356f40c96b34805da4b4fe1cf2e31fdf0
kosei <abdelhamid.boudjit@gmail.com>
2023-08-15 21:06:59 +0100

    edit the height

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -3,12 +3,14 @@ import { Link } from "react-router-dom";
 import Tabs from "../components/Tabs/Tabs";
 import { H1, Container, A, TextButton } from "../ui";
 import Range from "../components/Range/Range";
+import Info from "../components/Info/Info";
 
 function Home() {
   const [difficulty, setDifficulty] = useState<number>(0);
   const [wordLength, setWordLength] = useState<number>(3);
+  const [showInfo, setShowInfo] = useState(false);
   return (
-    <Container className="flex flex-col items-center justify-between gap-8 sm:gap-12 mt-5 sm:mt-10">
+    <Container className="flex flex-col items-center justify-between gap-8 sm:gap-12 py-24 min-h-[calc(100vh-20px)] max-h-screen">
       <H1>
         Welcome To <span className="text-pink-500">Words</span> Game
       </H1>
@@ -31,7 +33,8 @@ function Home() {
       <Link to="/game" state={{ difficulty, wordLength }}>
         <TextButton>Start!</TextButton>
       </Link>
-      <A>Don't know how to play ?</A>
+      <A onClick={() => setShowInfo(true)}>Don't know how to play ?</A>
+      {showInfo && <Info setShowInfo={setShowInfo} />}
     </Container>
   );
 }

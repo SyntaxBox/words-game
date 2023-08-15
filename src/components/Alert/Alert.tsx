8de937432e829cec523d1396d3f1f5620075b025
kosei <abdelhamid.boudjit@gmail.com>
2023-08-15 21:03:15 +0100

    Update Alert.tsx

--- src/components/Alert/Alert.tsx
+++ src/components/Alert/Alert.tsx
@@ -1,25 +1,26 @@
+import { ReactNode } from "react";
 import { IconAlertTriangle } from "@tabler/icons-react";
 import { iconsSize, iconStroke } from "../../shared";
 
 type props = {
   title: string;
-  description: string;
+  children: ReactNode;
   className?: string;
 } & JSX.IntrinsicElements["div"];
 
-function Alert({ title, description, className }: props) {
+function Alert({ title, children, className }: props) {
   return (
     <div
-      className={`max-w-[400px] bg-pink-100 dark:bg-[#ec489a68] border-t-4 border-pink-500 dark:border-pink-600 rounded-b-md text-pink-900 dark:text-pink-200 px-4 py-3 shadow-md ${className}`}
+      className={`w-full sm:max-w-[400px] bg-pink-100 dark:bg-[#692b58] border-t-4 border-pink-500 dark:border-pink-600 sm:rounded-b-md text-pink-900 dark:text-pink-200 px-4 py-3 shadow-md ${className}`}
       role="alert"
     >
       <div className="flex">
-        <div className="py-1 pr-4 text-pink-500 dark:text-pink-500 ">
+        <div className="py-1 pr-3 text-pink-500 dark:text-pink-500 ">
           <IconAlertTriangle size={iconsSize + 8} stroke={iconStroke} />
         </div>
         <div>
           <p className="font-bold">{title}</p>
-          <p className="text-sm">{description}</p>
+          <div className="text-sm">{children}</div>
         </div>
       </div>
     </div>

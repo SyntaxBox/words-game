e1baca14d5280187408a787bf85f99b892652eb8
kosei <abdelhamid.boudjit@gmail.com>
2023-08-16 22:12:31 +0100

    Create H2.test.tsx

---
+++ src/ui/typography/H2.test.tsx
@@ -0,0 +1,10 @@
+import { render } from "@testing-library/react";
+import H2 from "./H2";
+
+describe("H2 Component", () => {
+  it("should render the children correctly", () => {
+    const { getByText } = render(<H2>Hello World</H2>);
+    const H2Element = getByText("Hello World");
+    expect(H2Element).toBeInTheDocument();
+  });
+});

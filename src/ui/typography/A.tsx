1afed026117421156487887741721b103692acde
Abdelhamid Boudjit <qhamid@proton.me>
2024-06-11 18:49:01 +0100

    refactor: remove unused code

--- src/ui/typography/A.tsx
+++ src/ui/typography/A.tsx
@@ -1,14 +1,8 @@
-import { ReactNode } from "react";
-
-type props = {
-  children: ReactNode;
-} & JSX.IntrinsicElements["button"];
-
-export function A({ children, ...rest }: props) {
+export function A({ children, ...rest }: JSX.IntrinsicElements["button"]) {
   return (
     <button
-      className="inline-flex justify-center items-center gap-2 rounded-md font-semibold text-pink-500"
       {...rest}
+      className="inline-flex justify-center items-center gap-2 rounded-md font-semibold text-pink-500"
     >
       {children}
     </button>
